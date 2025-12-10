1943ecfe97e349d407668c4d1469d57df79b935b
Andreas Avgoustis <andreasav@alfaedu.gr>
2025-12-10 10:19:17 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+# Global Constants
+SEED = 42           # Random seed for reproducibility
+NUM_NODES = 100     # Number of sensor nodes
+AREA_SIZE = 1.0     # Size of the simulation field (1x1)
